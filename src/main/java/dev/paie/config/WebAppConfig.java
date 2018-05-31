f73f92b7b3a80d6810568b3aa081aff543ecf60c
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-05-31 12:11:45 +0200

    retour à H2

--- src/main/java/dev/paie/config/WebAppConfig.java
+++ src/main/java/dev/paie/config/WebAppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan({ "dev.paie.web", "dev.paie.web.controller", "dev.paie.web.listener" })
-@Import({ ServicesConfig.class, DataSourceMySQLConfig.class, SecurityConfig.class })
+@Import({ ServicesConfig.class, H2Config.class, SecurityConfig.class })
 public class WebAppConfig {
 
 	@Bean
